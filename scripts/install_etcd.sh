954019809cb97e0ba8c8fc39ef73e84f4cc80267
popeye <popeye_rs@163.com>
2020-10-17 19:09:44 +0800

    etcdpasswd config

--- scripts/install_etcd.sh
+++ scripts/install_etcd.sh
@@ -16,3 +16,34 @@ sudo cp etcdctl /usr/local/bin/
 rm -rf etcd-$ETCD_VERSION-linux-amd64
 
 etcdctl version
+
+# etcd configure file
+ETCD_CONFIG=/etc/etcd.conf
+echo "SELF_IP=$SELF_IP" >> $ETCD_CONFIG
+echo "NODE_1_IP=$SELF_IP" >> $ETCD_CONFIG
+
+# TODO fix systemd https://docs.portworx.com/reference/knowledge-base/etcd-quick-setup/
+ETCD_SYSTEMD=/etc/systemd/system/etcd3.service
+cat << EOF >$ETCD_SYSTEMD
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos/etcd
+Conflicts=etcd.service
+Conflicts=etcd2.service
+
+[Service]
+Type=notify
+Restart=always
+RestartSec=25s
+LimitNOFILE=40000
+TimeoutStartSec=20s
+EnvironmentFile=/etc/etcd.conf
+ExecStart=/bin/sh -c "etcd --name etcd-${SELF_IP} --data-dir /var/lib/etcd --quota-backend-bytes 8589934592 --auto-compaction-retention 3 --listen-client-urls http://${SELF_IP}:2379,http://localhost:2379 --advertise-client-urls http://${SELF_IP}:2379,http://localhost:2379 --listen-peer-urls http://${SELF_IP}:2380 --initial-advertise-peer-urls http://${SELF_IP}:2380 --initial-cluster 'etcd-${NODE_1_IP}=http://${NODE_1_IP}:2380' --initial-cluster-token my-etcd-token --initial-cluster-state new"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable etcd3
+sudo systemctl start etcd3
--- scripts/install_etcdpasswd.sh
+++ scripts/install_etcdpasswd.sh
@@ -6,3 +6,41 @@ set -eu
 
 go get -u github.com/cybozu-go/etcdpasswd/...
 
+
+# etcdpasswd config
+ETCDPASSWD_CONFIG=/etc/etcdpasswd/config.yml
+mkdir -p /etc/etcdpasswd/ && touch $ETCDPASSWD_CONFIG
+cat << EOF >$ETCDPASSWD_CONFIG
+endpoints:
+  - http://${ETCD_IP}:2379
+username: ${ETCD_USERNAME}
+password: ${ETCD_PASSWORD}
+
+#tls-cert-file: /etc/etcdpasswd/etcd.crt
+#tls-key-file: /etc/etcdpasswd/etcd.key
+EOF
+
+sudo cp $GOPATH/bin/ep-agent /usr/local/sbin
+
+# systemd
+ETCDPASSWD_SYSTEMD=/etc/systemd/system/ep-agent.service
+cat << EOF >$ETCDPASSWD_SYSTEMD
+[Unit]
+Description=etcdpasswd sync agent
+After=network-online.target
+Wants=network-online.target
+ConditionPathExists=/etc/etcdpasswd/config.yml
+
+[Service]
+Type=simple
+Restart=on-failure
+RestartForceExitStatus=SIGPIPE
+ExecStart=/usr/local/sbin/ep-agent -logformat json
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable ep-agent.service
+sudo systemctl start ep-agent.service
