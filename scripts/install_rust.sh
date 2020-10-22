0cc09ecda661dd3df5d9bac6e625b833ff303324
popeye <popeye_rs@163.com>
2020-10-22 18:46:02 +0800

    update script

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -2,39 +2,10 @@
 
 set -eu
 
-# Install rust toolchain
-export RUSTUP_HOME=/usr/local/rustup
-export CARGO_HOME=/usr/local/cargo
-export PATH=/usr/local/cargo/bin:$PATH
-
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --no-modify-path -y
-
-chmod -R a+w+w /usr/local/cargo/
-chmod -R a+w+w /usr/local/rustup/
-
-rustup install nightly-2020-10-06
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
-
-# Auto upgrade at midnight, disable auto upgrade nightly is broken.
-# no need to update if we reset server every night.
-# CRON_CONFIG=/root/crontab_temp
-# touch $CRON_CONFIG
-# echo "0 0 * * * /usr/local/cargo/rustup update >/root/rustup-update.log 2>&1;chmod -R a+w+r /usr/local/cargo/;chmod -R a+w+r /usr/local/rustup/;" >> $CRON_CONFIG
-# crontab $CRON_CONFIG
-# rm $CRON_CONFIG
-
-# Enable rustup and cargo for all users
-PROFILE_FILE=/etc/profile
-echo "export PATH=/usr/local/cargo/bin:$PATH" >> $PROFILE_FILE
-echo "export RUSTUP_HOME=/usr/local/rustup" >> $PROFILE_FILE
-echo "export CARGO_HOME=/usr/local/cargo" >> $PROFILE_FILE
-
-# ===================================
 # Install Rust for each user
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --no-modify-path -y
+USER_NAME=sample3
 
-source $HOME/.cargo/env
+su - $USER_NAME -c "curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --no-modify-path -y"
 
-rustup install nightly-2020-10-06
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
+su - $USER_NAME -c "rustup install nightly-2020-10-05; rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-05"
